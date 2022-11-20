ee94b04a6c14f2dd4e2b077d85a61b73a17a6dea
ilya udalov <aetoneilya@laptop.localdomain>
2022-11-21 00:59:57 +0700

    init of ant algorithm

--- src/graph_algorithms/s21_graph_algorithms.h
+++ src/graph_algorithms/s21_graph_algorithms.h
@@ -3,11 +3,19 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
 
 #include "../graph/s21_graph.h"
 
+struct TsmResult {
+  std::vector<int> vertices;  // an array with the route you are looking for
+  double distance;            // the length of this route
+};
+
 class GraphAlgoritms {
  public:
+  TsmResult solveTravelingSalesmanProblem(Graph& graph);
+
  private:
 };
 
