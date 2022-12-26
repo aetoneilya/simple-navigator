ac50b7ef425576b81f72e81e9c87dbcdd40df356
ilya udalov <aetoneilya@laptop.localdomain>
2022-12-27 02:28:10 +0700

    fix

--- src/main.cc
+++ src/main.cc
@@ -4,12 +4,13 @@
 
 int main() {
   s21::Graph my_graph;
-  my_graph.LoadGraphFromFile("graph.txt");
+  my_graph.LoadGraphFromFile("graph_2.txt");
   //   my_graph.add_edge(0, 0, 11);
   my_graph.Draw();
   s21::GraphAlgoritms ga;
-  //   tsm::TsmResult res = ga.SolveTravelingSalesmanProblem(my_graph);
-  s21::tsm::TsmResult res = ga.NaiveSolveTravelingSalesmanProblem(my_graph);
+  s21::tsm::TsmResult res = ga.SolveTravelingSalesmanProblem(my_graph);
+  //   s21::tsm::TsmResult res =
+  //   ga.NaiveSolveTravelingSalesmanProblem(my_graph);
 
   std::cout << "distance: " << res.distance << std::endl;
   for (auto city : res.vertices) std::cout << city << ' ';
